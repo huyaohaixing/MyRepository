405046ec0782f58136443f6cdf25aacc7f667548
huyaohaixing <huyaohaixing@sina.com>
2019-07-08 19:37:52 +0800

    master-test1

---
+++ test1.java
@@ -0,0 +1,11 @@
+package cn.itcast.git;
+
+import org.junit.Test;
+
+public class test1 {
+    @Test
+    public void test(){
+        //test1-master
+        System.out.println("adasdasda");
+    }
+}
